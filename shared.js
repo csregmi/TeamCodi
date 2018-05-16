464b1e110d4812397379cecfec7380a2bf093f7a
Jeffrey Navarro <32873029+JeffreyReyesNavarro@users.noreply.github.com>
2018-05-16 17:33:03 +0930

    Update shared.js

--- shared.js
+++ shared.js
@@ -1,10 +1,13 @@
+// TOGGLEBUTTON
 var backdrop = document.querySelector('.backdrop');
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
 
+//CARD EXPAND AND COLLAPSE
+var expandBtn = document.getElementsByClassName('button1');
+var cardDescription = document.querySelector('.main-content__description')
 
-
-// OPEN AND CLOSE TOGGLE MENU
+// TOGGLEBUTTON
 toggleButton.addEventListener('click', openBackdrop);
 backdrop.addEventListener('click', closeBackdrop);
 
@@ -19,3 +22,21 @@ function openBackdrop() {
     backdrop.style.display = 'block';
     mobileNav.style.display = 'block';
 }
+
+//CARD EXPAND AND COLLAPSE
+for (var i = 0; i < expandBtn.length; i++) {
+    document.getElementsByClassName('button1')[i].addEventListener('click', function () { //GET INDEX OF BTN
+        var index = this.getAttribute('data-id');
+      
+        var display = document.getElementsByClassName('main-content__description')[index];
+
+        if (display.style.display == "none" || display.style.display == '') { //CHECK DISPLAY VALUE
+            display.style.display = 'block';
+        } else {
+            display.style.display = 'none';
+        }
+    })
+}
+
+
+
